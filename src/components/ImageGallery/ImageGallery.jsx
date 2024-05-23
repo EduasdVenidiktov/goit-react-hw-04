ed7e699ec6a2c6c84783cfd3ef6b00f334ae9e2e
Ed Ven <edven7777777@gmail.com>
2024-05-23 13:41:01 +0300

    without commented cod

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -18,25 +18,3 @@ const ImageGallery = ({ images, onImageClick }) => {
 };
 
 export default ImageGallery;
-
-//=====================================================================================
-// import ImageCard from "../ImageCard/ImageCard";
-// import css from "./ImageGallery.module.css";
-
-// const ImageGallery = ({ images }) => {
-//   return (
-//     <ul className={css.itemList}>
-//       {images.map((image) => (
-//         <li key={image.id}>
-//           <ImageCard
-//             photo={image.urls.small}
-//             largePhoto={image.urls.regular}
-//             description={image.alt_description}
-//           ></ImageCard>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ImageGallery;
